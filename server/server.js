1b2393a8946dc0749494cdfe124f1e840f885ddc
Tom Lee <tlee2951@gmail.com>
2018-11-29 09:21:23 -0700

    Amended - Add server.js

---
+++ server/server.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    completedAt: {
+        type: Number,
+        default: null
+    }
+});
+
+// var newTodo = new Todo({
+//     text: 'Something to do'
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log('Saved todo', doc);
+// }, (e) => {
+//     console.log('Unable to save todo', e);
+// });
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true
+    }
+});
+
+var newUser = new User({
+    email: 'myemail@me.com'
+});
+
+newUser.save().then((doc) => {
+    console.log('Saved user', doc);
+}, (e) => {
+    console.log('Unable to save user', e);
+})
